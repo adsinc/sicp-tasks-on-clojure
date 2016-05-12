d8368bc5562507100e1de159a13a520551c7f680
Aleksei Dolgii <sinc86@gmail.com>
2016-05-12 23:10:55 +0300

    sqrt

--- src/sicp_tasks_on_clojure/chapter1.clj
+++ src/sicp_tasks_on_clojure/chapter1.clj
@@ -1,10 +1,10 @@
 (ns sicp-tasks-on-clojure.chapter1)
 
 (defn good-enougth? [guess x]
-  (< (abs (- (* guess guess) x)) 0.001))
+  (< (Math/abs (- (Math/pow guess 2) x)) 0.001))
 
-(def average [x y]
-  (/ (+ x y) 2))
+(defn average [x y]
+  (/ (+ x y) 2.0))
 
 (defn improve [guess x]
   (average guess (/ x guess)))
@@ -15,3 +15,5 @@
     (sqrt-iter (improve guess x)
                x)))
 
+(defn sqrt [x]
+  (sqrt-iter 1 x))
