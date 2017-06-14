7578c657d8d7f9dd4284d6061ed042048effc624
Aleksei Dolgii <sinc86@gmail.com>
2017-06-14 21:36:10 +0300

    1_3_3

---
+++ src/sicp_tasks_on_clojure/chapter1_3_3.clj
@@ -0,0 +1,19 @@
+(ns sicp-tasks-on-clojure.chapter1-3-3)
+
+(def tolerance 0.00001)
+
+(defn fixed-point [f first-guess]
+  (letfn [(close-enough? [v1 v2]
+            (< (Math/abs (- v1 v2)) tolerance))
+          (try-count [guess]
+            (println guess)
+            (let [next (f guess)]
+              (if (close-enough? guess next)
+                next
+                (try-count next))))]
+    (try-count first-guess)))
+
+(defn avg [x y] (/ (+ x y) 2))
+
+(defn sqrt [x]
+  (fixed-point #(avg % (/ x %)) 1.0))
