4b304106957f5949dbd8afff8d12cceb746e3a37
Aleksei Dolgii <sinc86@gmail.com>
2017-06-20 22:02:53 +0300

    1_3_4

--- src/sicp_tasks_on_clojure/chapter1_3_4.clj
+++ src/sicp_tasks_on_clojure/chapter1_3_4.clj
@@ -7,3 +7,35 @@
 
 (defn cube-root [x]
   (fixed-point (average-damp #(/ x (square %))) 1.0))
+
+(def dx 0.00001)
+
+(defn deriv [g]
+  (fn [x] (/ (- (g (+ x dx)) (g x))
+             dx)))
+
+(defn cube [x] (* x x x))
+
+(defn newton-transform [g]
+  (fn [x]
+    (- x (/ (g x) ((deriv g) x)))))
+
+(defn newton-method [g guess]
+  (fixed-point (newton-transform g) guess))
+
+;(defn sqrt [x]
+;  (newton-method (fn [y] (- (square y) x))
+;                 1.0))
+
+(defn fixed-point-of-transform [g transform guess]
+  (fixed-point (transform g) guess))
+
+;(defn sqrt [x]
+;  (fixed-point-of-transform (fn [y] (/ x y))
+;                            average-damp
+;                            1.0))
+
+(defn sqrt [x]
+  (fixed-point-of-transform (fn [y] (- (square y) x))
+                            newton-transform
+                            1.0))
